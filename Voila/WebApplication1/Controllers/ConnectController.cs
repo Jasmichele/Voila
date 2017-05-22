8538146b7adbbac461258d208acad3db031e3c05
Jasmichele <jasmichele@yahoo.com>
2017-05-22 13:56:29 -0400

    5-22 11am

---
+++ Voila/WebApplication1/Controllers/ConnectController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ConnectController : Controller
+    {
+        VoilaEntities db = new VoilaEntities();
+        // GET: Connect
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetByFilter()
+        {
+            ConnectModel looking = new ConnectModel();
+
+            VoilaEntities db = new VoilaEntities();
+            looking.City = db.Cities.ToList();
+            looking.Proffesion = db.Proffesions.ToList();
+            looking.Price = db.Prices.ToList();
+
+            return View(looking);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetByFilter(FormCollection form)
+        {
+
+        }
+
+    }
+}
---
+++ Voila/WebApplication1/Models/ConnectModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ConnectModel
+    {
+        public IEnumerable<City> City { get; set; }
+        public IEnumerable<Proffesion> Proffesion { get; set; }
+        public IEnumerable<Price> Price { get; set; }
+        
+        public int CityID { get; set; }
+        public int ProffesionId { get; set; }
+        public int PriceId { get; set; }
+    }
+}
