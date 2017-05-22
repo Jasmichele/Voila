6a6f39da8c80395ce3cf07bfedf0fad2fd12fff0
Jasmichele <jasmichele@yahoo.com>
2017-05-22 18:18:10 -0400

    3:20

--- Voila/WebApplication1/Controllers/ConnectController.cs
+++ Voila/WebApplication1/Controllers/ConnectController.cs
@@ -21,19 +21,45 @@ namespace WebApplication1.Controllers
             ConnectModel looking = new ConnectModel();
 
             VoilaEntities db = new VoilaEntities();
-            looking.City = db.Cities.ToList();
-            looking.Proffesion = db.Proffesions.ToList();
+            looking.Cities = db.Cities.ToList();
+            looking.Proffesions = db.Proffesions.ToList();
             looking.Price = db.Prices.ToList();
 
             return View(looking);
         }
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult GetByFilter(FormCollection form)
         {
+            string cit = form["City"].ToString();
+            string pro = form["Proffesion"].ToString();
+            string pri = form["Prie"].ToString();
 
+            return RedirectToAction("GetUsers", new { cid = cit, pid = pro, prid = pri });
         }
 
+        public ActionResult GetUsers(int? cid, int? pid, int? prid)
+        {
+            var pros = from pr in db.Profiles
+                       select pr;
+
+            var filteredPros = pros;
+
+            int tempCid = Convert.ToInt32(cid);
+            int tempPid = Convert.ToInt32(pid);
+            int tempPrid = Convert.ToInt32(prid);
+
+            if (cid != null)
+                filteredPros = filteredPros.Where(s => s.CityID == tempCid);
+
+            if (pid != null)
+                filteredPros = filteredPros.Where(s => s.ProffesionID == tempPid);
+
+            if (prid != null)
+                filteredPros = filteredPros.Where(s => s.PriceID == tempPrid);
+
+            return View(filteredPros);
+        }
     }
 }
--- Voila/WebApplication1/Models/ConnectModel.cs
+++ Voila/WebApplication1/Models/ConnectModel.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace WebApplication1.Models
 {
     public class ConnectModel
     {
-        public IEnumerable<City> City { get; set; }
-        public IEnumerable<Proffesion> Proffesion { get; set; }
+        public IEnumerable<City> Cities { get; set; }
+        public IEnumerable<Proffesion> Proffesions { get; set; }
         public IEnumerable<Price> Price { get; set; }
         
-        public int CityID { get; set; }
+     
+        public int CityId { get; set; }
         public int ProffesionId { get; set; }
         public int PriceId { get; set; }
     }
